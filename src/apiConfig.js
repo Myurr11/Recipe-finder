b7caf5c9daa3f400aa178c33fdcb6399e17f92e7
Myurr11 <myurjoshi00742@gmail.com>
2024-11-22 23:11:02 +0530

    Enhanced recipe cards and main page, SearchByIngredients yet to feature same enhancements

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -1,4 +1,2 @@
 export const API_KEY = "1ba98f347dff40cf8ada792e2d41b069";
 export const BASE_URL = "https://api.spoonacular.com/recipes";
-
-export const OPENAI_API_KEY = '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'; 
